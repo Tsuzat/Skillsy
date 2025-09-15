4fbd1a5a02688fb41e22716fc8623f68a63fc049
Tsuzat <dev.tsuzat@gmail.com>
2025-09-15 23:41:08 +0530

    feat(utils): add getUserInitals function to generate user initials

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -11,3 +11,19 @@ export type WithoutChild<T> = T extends { child?: any } ? Omit<T, 'child'> : T;
 export type WithoutChildren<T> = T extends { children?: any } ? Omit<T, 'children'> : T;
 export type WithoutChildrenOrChild<T> = WithoutChildren<WithoutChild<T>>;
 export type WithElementRef<T, U extends HTMLElement = HTMLElement> = T & { ref?: U | null };
+
+export const getUserInitals = (userName?: string) => {
+	if (userName === undefined) return '??';
+	try {
+		const words = userName.split(' ');
+		if (words.length > 1) {
+			return words[0][0] + words[1][0];
+		} else if (words.length === 1 && words[0].length > 1) {
+			return words[0][0] + words[0][1];
+		} else {
+			return words[0][0];
+		}
+	} catch {
+		return '??';
+	}
+};
